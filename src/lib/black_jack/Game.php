e3c7cf0af7c550283c5905b5a3be1ffe8563cd82
riotaro <rugby117117@gmail.com>
2026-06-14 17:42:31 +0900

    first commit

---
+++ src/lib/black_jack/Game.php
@@ -0,0 +1,22 @@
+<?php
+
+echo 'ブラックジャックを開始します。'. PHP_EOL;
+
+echo "あなたの引いたカードは{$suit}の{$number}です。". PHP_EOL;
+echo "あなたの引いたカードは{$suit}の{$number}です。". PHP_EOL;
+echo "ディーラーの引いたカードは{$suit}の{$number}です。". PHP_EOL;
+echo "ディーラーの引いた２枚目のカードは分かりません。". PHP_EOL;
+echo "あなたの現在の得点は{$score}です。カードを引きますか？". '(Y/N)'. PHP_EOL;
+
+echo "あなたの引いたカードは{$suit}の{$number}です。". PHP_EOL;
+echo "あなたの現在の得点は{$score}です。カードを引きますか？". '(Y/N)'. PHP_EOL;
+
+echo "ディーラーの引いた２枚目のカードは{$suit}の{$number}でした。". PHP_EOL;
+echo "ディーラーの現在の得点は{$score}です。". PHP_EOL;
+echo "ディーラーの引いたカードは{$suit}の{$number}です。". PHP_EOL;
+
+echo "あなたの得点は{$score}です。". PHP_EOL;
+echo "ディーラーの得点は{$score}です。". PHP_EOL;
+echo "あなたの{$judge}です！". PHP_EOL;
+
+echo 'ブラックジャックを終了します。'. PHP_EOL;
