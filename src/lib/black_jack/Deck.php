a8d6dbf9cfff6c9e2461dd29f569dbced1f49746
riotaro <rugby117117@gmail.com>
2026-06-16 10:37:58 +0900

    シャッフルする関数を作成

---
+++ src/lib/black_jack/Deck.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once(__DIR__ . '/Card.php');
+
+class Deck
+{
+  public const ALL_SUITS = ['ハート', 'クラブ', 'スペード', 'ダイヤ'];
+  public const ALL_NUMBERS = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+  public $cards = [];
+
+  public function __construct()
+  {
+    foreach (self::ALL_SUITS as $suit) {
+      foreach (self::ALL_NUMBERS as $number) {
+        $this->cards[] = new Card($suit, $number);
+      }
+    }
+  }
+
+  public function shuffleDeck(): array
+  {
+    // ['HA','H2'......,'DK']合計52枚が整列して入ってくる
+    // 最後尾は0からスタートの51番目
+    for ($i = count($this->cards) - 1; $i > 0; $i--) {
+      $j = mt_rand(0, $i);
+      [$this->cards[$i], $this->cards[$j]] = [$this->cards[$j], $this->cards[$i]];
+    }
+    return $this->cards;
+  }
+
+  public function drawCard() {}
+  public function notDrawCard() {}
+}
