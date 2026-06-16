24ce6676e01af61321e1d13cbfc15b7f31fb7e0f
riotaro <rugby117117@gmail.com>
2026-06-17 05:47:21 +0900

    Cardクラス作成

---
+++ src/lib/black_jack/Card.php
@@ -0,0 +1,16 @@
+<?php
+namespace BlackJack;
+
+class Card
+{
+  public function __construct(public string $suit, public string $number) {}
+
+  public function getSuit(): string
+  {
+    return $this->suit;
+  }
+  public function getNumber(): string
+  {
+    return $this->number;
+  }
+}
