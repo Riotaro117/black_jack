74381a838bfa09ab98c1426b03ebf6adb4f2658b
riotaro <rugby117117@gmail.com>
2026-06-17 05:47:57 +0900

    テストの作成

---
+++ src/tests/black_jack/CardTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlackJack\Tests;
+
+use BlackJack\Card;
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../../lib/black_jack/Card.php');
+
+
+class CardTest extends TestCase
+{
+  public function testGetSuit(): void
+  {
+    $card = new Card('ハート', 'A');
+    $this->assertSame('ハート', $card->getSuit());
+  }
+  public function testGetNumber(): void
+  {
+    $card = new Card('ハート', 'A');
+    $this->assertSame('A', $card->getNumber());
+    $card = new Card('ハート', '2');
+    $this->assertSame('2', $card->getNumber());
+  }
+}
---
+++ src/tests/black_jack/DeckTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BlackJack\Tests;
+
+use BlackJack\Deck;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../../lib/black_jack/Deck.php');
+
+
+class DeckTest extends TestCase
+{
+  public function testShuffleKeepCards()
+  {
+    $deck = new Deck();
+    $this->assertCount(52, $deck->cards);
+  }
+  public function testShuffleDoesNotLoseOrDuplicate(): void
+  {
+    $deck = new Deck();
+    // シャッフルする前
+    $before = array_map(fn($card) => $card->suit . $card->number, $deck->cards);
+    sort($before);
+    // シャッフルした後
+    $deck->shuffleDeck();
+    $after = array_map(fn($card) => $card->suit . $card->number, $deck->cards);
+    sort($after);
+    $this->assertSame($before, $after);
+  }
+
+  public function testDrawCardReduce(): void
+  {
+    // カードが減っているか
+    $deck = new Deck();
+    $deck->drawCard();
+    $this->assertCount(
+      51,
+      $deck->cards
+    );
+  }
+  public function testDrawCardError(): void
+  {
+    // 52枚全てカードを引く
+    $deck = new Deck();
+    for ($i = 0; $i < 52; $i++) {
+      $deck->drawCard();
+    }
+    // 53枚目で例外の処理
+    $this->expectException(LogicException::class);
+    $deck->drawCard();
+  }
+}
