f97e92aa5571d6743d24b4ad3b82d12520735c6d
riotaro <rugby117117@gmail.com>
2026-06-17 05:47:43 +0900

    Gameクラス作成

--- src/lib/black_jack/Game.php
+++ src/lib/black_jack/Game.php
@@ -1,22 +1,34 @@
 <?php
 
-echo 'ブラックジャックを開始します。'. PHP_EOL;
+namespace BlackJack;
 
-echo "あなたの引いたカードは{$suit}の{$number}です。". PHP_EOL;
-echo "あなたの引いたカードは{$suit}の{$number}です。". PHP_EOL;
-echo "ディーラーの引いたカードは{$suit}の{$number}です。". PHP_EOL;
-echo "ディーラーの引いた２枚目のカードは分かりません。". PHP_EOL;
-echo "あなたの現在の得点は{$score}です。カードを引きますか？". '(Y/N)'. PHP_EOL;
+class Game
+{
 
-echo "あなたの引いたカードは{$suit}の{$number}です。". PHP_EOL;
-echo "あなたの現在の得点は{$score}です。カードを引きますか？". '(Y/N)'. PHP_EOL;
 
-echo "ディーラーの引いた２枚目のカードは{$suit}の{$number}でした。". PHP_EOL;
-echo "ディーラーの現在の得点は{$score}です。". PHP_EOL;
-echo "ディーラーの引いたカードは{$suit}の{$number}です。". PHP_EOL;
+  public function start(): void
+  {
+    echo 'ブラックジャックを開始します。' . PHP_EOL;
 
-echo "あなたの得点は{$score}です。". PHP_EOL;
-echo "ディーラーの得点は{$score}です。". PHP_EOL;
-echo "あなたの{$judge}です！". PHP_EOL;
+    echo "あなたの引いたカードは{}の{}です。" . PHP_EOL;
+    echo "あなたの引いたカードは{}の{}です。" . PHP_EOL;
+    echo "ディーラーの引いたカードは{}の{}です。" . PHP_EOL;
+    echo "ディーラーの引いた２枚目のカードは分かりません。" . PHP_EOL;
+    echo "あなたの現在の得点は{}です。カードを引きますか？" . '(Y/N)' . PHP_EOL;
 
-echo 'ブラックジャックを終了します。'. PHP_EOL;
+    echo "あなたの引いたカードは{}の{}です。" . PHP_EOL;
+    echo "あなたの現在の得点は{}です。カードを引きますか？" . '(Y/N)' . PHP_EOL;
+
+    echo "ディーラーの引いた２枚目のカードは{}の{}でした。" . PHP_EOL;
+    echo "ディーラーの現在の得点は{}です。" . PHP_EOL;
+    echo "ディーラーの引いたカードは{}の{}です。" . PHP_EOL;
+
+    echo "あなたの得点は{}です。" . PHP_EOL;
+    echo "ディーラーの得点は{}です。" . PHP_EOL;
+    echo "あなたの{}です！" . PHP_EOL;
+
+    echo 'ブラックジャックを終了します。' . PHP_EOL;
+
+    return;
+  }
+}
