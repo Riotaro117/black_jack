3126f11bccd8404590c548b0d8b4c7ef7accf09e
riotaro <rugby117117@gmail.com>
2026-06-19 14:02:47 +0900

    ディーラーのクラス作成

---
+++ src/lib/black_jack/Dealer.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BlackJack;
+
+require_once(__DIR__ . '/Deck.php');
+require_once(__DIR__ . '/TotalScore.php');
+class Dealer
+{
+  public $dealer_hand = [];
+
+  // 山札をシャッフルする
+  public function shuffleDeck(): array
+  {
+    $deck = new Deck();
+    return $deck->shuffleDeck();
+  }
+
+  // 手札の点数を合計する
+  public function totalScore(): int
+  {
+    $total_score = new TotalScore();
+    return $total_score->totalScore($this->dealer_hand);
+  }
+
+  // 手札にカードを1枚加える
+  public function addCardMyHand(): void
+  {
+    $deck = new Deck();
+    $draw_card = $deck->drawCard();
+    $this->dealer_hand[] = $draw_card;
+  }
+
+  // 直前に加えたカードの情報を取得する
+  public function getPreviousCard(): Card
+  {
+    if ($this->dealer_hand === []) {
+      throw new \LogicException('手札にカードがありません。');
+    }
+    $previous_card = array_pop($this->dealer_hand);
+    return $previous_card;
+  }
+
+  // 合計が17点以上になるまでカードを引き続ける
+  public function keepDrawing(): void
+  {
+    while ($this->totalScore() < 17) {
+      // $this->totalScore()で毎回関数を読んでいるので、自動で計算が行われている
+      $this->addCardMyHand();
+    }
+  }
+}
---
+++ src/tests/black_jack/DealerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BlackJack\Tests;
+
+use BlackJack\Card;
+use BlackJack\Dealer;
+use BlackJack\TotalScore;
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../../lib/black_jack/Dealer.php');
+
+
+class DealerTest extends TestCase
+{
+  public function testKeepDrawing(): void
+  {
+    // 17点以上なら引かない
+    $dealer = new Dealer();
+    $dealer->dealer_hand = [new Card('ハート', 'K'), new Card('ダイヤ', 'Q')];
+    $total_score = new TotalScore();
+    $before_score1 = $total_score->totalScore($dealer->dealer_hand);
+    $dealer->keepDrawing();
+    $after_score = $total_score->totalScore($dealer->dealer_hand);
+    $this->assertSame($before_score1, $after_score);
+
+    // 17点未満ならカードが増える
+    $dealer->dealer_hand = [new Card('ハート', '6'), new Card('ダイヤ', 'Q')];
+    $dealer->keepDrawing();
+    $this->assertSame(3, count($dealer->dealer_hand));
+  }
+}
