8452485935a0a0bdf1e67c18c3e0a96ffd8a2126
riotaro <rugby117117@gmail.com>
2026-06-22 15:00:00 +0900

    ゲームの流れをクラスを呼んで実行

--- src/lib/black_jack/Game.php
+++ src/lib/black_jack/Game.php
@@ -2,30 +2,86 @@
 
 namespace BlackJack;
 
+require_once(__DIR__ . '/PlayerA.php');
+require_once(__DIR__ . '/Dealer.php');
+require_once(__DIR__ . '/Message.php');
+require_once(__DIR__ . '/Rule.php');
+
 class Game
 {
-
-
   public function start(): void
   {
+    $deck = new Deck();
+    $player = new PlayerA();
+    $dealer = new Dealer();
+    $message = new Message();
+    $rule = new Rule();
     echo 'ブラックジャックを開始します。' . PHP_EOL;
 
-    echo "あなたの引いたカードは{}の{}です。" . PHP_EOL;
-    echo "あなたの引いたカードは{}の{}です。" . PHP_EOL;
-    echo "ディーラーの引いたカードは{}の{}です。" . PHP_EOL;
-    echo "ディーラーの引いた２枚目のカードは分かりません。" . PHP_EOL;
-    echo "あなたの現在の得点は{}です。カードを引きますか？" . '(Y/N)' . PHP_EOL;
+    // 山札をシャッフルする
+    $dealer->shuffleDeck($deck);
 
-    echo "あなたの引いたカードは{}の{}です。" . PHP_EOL;
-    echo "あなたの現在の得点は{}です。カードを引きますか？" . '(Y/N)' . PHP_EOL;
+    // 1回目プレイヤーがカードを引く
+    $player->addCardMyHand($deck);
+    // 1回目引いたカードを表示する
+    echo $message->drawCardMessage($player) . PHP_EOL;
 
-    echo "ディーラーの引いた２枚目のカードは{}の{}でした。" . PHP_EOL;
-    echo "ディーラーの現在の得点は{}です。" . PHP_EOL;
-    echo "ディーラーの引いたカードは{}の{}です。" . PHP_EOL;
+    // 2回目プレイヤーがカードを引く
+    $player->addCardMyHand($deck);
+    // 2回目引いたカードを表示する
+    echo $message->drawCardMessage($player) . PHP_EOL;
 
-    echo "あなたの得点は{}です。" . PHP_EOL;
-    echo "ディーラーの得点は{}です。" . PHP_EOL;
-    echo "あなたの{}です！" . PHP_EOL;
+    // 1回目ディーラーがカードを引く
+    $dealer->addCardMyHand($deck);
+    // 1回目引いたカードを表示する
+    echo $message->drawCardMessage($dealer) . PHP_EOL;
+
+    // 2回目ディーラーがカードを引く
+    $dealer->addCardMyHand($deck);
+    // 2回目ディーラーが引いたカードは表示しない
+    echo $message->dealerDrawSecondCardMessage($dealer, 'second') . PHP_EOL;
+
+
+
+
+    
+    // プレイヤーの得点を表示して、カードを引くか聞く（ループが必要かも）TODO
+    echo $message->askDrawCardMessage($player) . PHP_EOL;
+
+    // Yならカードを1枚ひく、Nならカードを引かない
+    $response = $player->decideDrawAnotherCard();
+
+    $rule->addAnotherCard($response, $player, $deck);
+
+    // 引いたカードを表示する
+    echo $message->drawCardMessage($player) . PHP_EOL;
+
+    // プレイヤーの得点を表示して、カードを引くか聞く
+    // 21点以上ならカードを引くかどうか聞かなくてもいい
+    echo $message->askDrawCardMessage($player) . PHP_EOL;
+
+    // Yならカードを1枚ひく、Nならカードを引かない
+    $response = $player->decideDrawAnotherCard();
+
+    $rule->addAnotherCard($response, $player, $deck);
+
+    // ディーラーが2枚目に引いたカードを表示する
+    echo $message->dealerDrawSecondCardMessage($dealer, 'second') . PHP_EOL;
+
+    // ディーラーの現在の得点を表示する
+    echo $message->pointMessage($dealer) . PHP_EOL;
+    // ディーラーは17点以上になるようにカードを引き続ける
+    $dealer->keepDrawing($deck);
+    // 引いたカードを表示する(引きまくったら最後のカードしか表示されない)
+    echo $message->drawCardMessage($dealer) . PHP_EOL;
+
+    // 結果発表
+    echo $message->pointMessage($player) . PHP_EOL;
+    echo $message->pointMessage($dealer) . PHP_EOL;
+
+    $result = $rule->checkNotOverCloseTo21Points($dealer, $player);
+
+    echo $message->resultMessage($result) . PHP_EOL;
 
     echo 'ブラックジャックを終了します。' . PHP_EOL;
 
