07450d0958d16f718dc22c38405d4a8b2458332a
riotaro <rugby117117@gmail.com>
2026-06-22 15:01:34 +0900

    引数でdeckを受け取る形に変更

--- src/tests/black_jack/DealerTest.php
+++ src/tests/black_jack/DealerTest.php
@@ -4,6 +4,7 @@ namespace BlackJack\Tests;
 
 use BlackJack\Card;
 use BlackJack\Dealer;
+use BlackJack\Deck;
 use BlackJack\TotalScore;
 use PHPUnit\Framework\TestCase;
 
@@ -15,17 +16,18 @@ class DealerTest extends TestCase
   public function testKeepDrawing(): void
   {
     // 17点以上なら引かない
+    $deck = new Deck();
     $dealer = new Dealer();
     $dealer->dealer_hand = [new Card('ハート', 'K'), new Card('ダイヤ', 'Q')];
     $total_score = new TotalScore();
     $before_score1 = $total_score->totalScore($dealer->dealer_hand);
-    $dealer->keepDrawing();
+    $dealer->keepDrawing($deck);
     $after_score = $total_score->totalScore($dealer->dealer_hand);
     $this->assertSame($before_score1, $after_score);
 
     // 17点未満ならカードが増える
     $dealer->dealer_hand = [new Card('ハート', '6'), new Card('ダイヤ', 'Q')];
-    $dealer->keepDrawing();
+    $dealer->keepDrawing($deck);
     $this->assertSame(3, count($dealer->dealer_hand));
   }
 }
