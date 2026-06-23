b5b676fbb55d10e4a1819508c0f624d70757c82b
riotaro <rugby117117@gmail.com>
2026-06-23 09:47:37 +0900

    勝利の条件を変更

--- src/lib/black_jack/Rule.php
+++ src/lib/black_jack/Rule.php
@@ -9,36 +9,30 @@ class Rule
 {
   private function winOfPlayer(int $dealer_total_score, int $player_total_score): bool
   {
-    return (21 - $dealer_total_score) > (21 - $player_total_score) || $dealer_total_score > 21;
+    return (21 - $dealer_total_score) > (21 - $player_total_score);
   }
   private function winOfDealer(int $dealer_total_score, int $player_total_score): bool
   {
-    return (21 - $dealer_total_score) < (21 - $player_total_score) || $player_total_score > 21;
+    return (21 - $dealer_total_score) < (21 - $player_total_score);
   }
 
-
-  // Yesならカードを追加で引く、Nならカードを引かない
-  public function addAnotherCard(string $response, PlayerA $player, Deck $deck): void
-  {
-    // Playerは外部からの要素なので新規に作成すると別のプレイヤーになってしまう
-    if ($response === 'Y') {
-      $player->addCardMyHand($deck);
-    } else {
-      return;
-    }
-  }
 
   // どちらのスコアが21点に近いかチェック
   public function checkNotOverCloseTo21Points(Dealer $dealer, PlayerA $player): string
   {
     $dealer_total_score = $dealer->totalScore();
     $player_total_score = $player->totalScore();
-    if ($this->winOfPlayer($dealer_total_score, $player_total_score)) {
+
+    if ($player_total_score > 21) {
+      return 'dealer';
+    } elseif ($dealer_total_score > 21) {
+      return 'player';
+    } elseif ($this->winOfPlayer($dealer_total_score, $player_total_score)) {
       return 'player';
     } elseif ($this->winOfDealer($dealer_total_score, $player_total_score)) {
       return 'dealer';
     } else {
-      return 'continue';
+      return 'draw';
     }
   }
 }
