8583456e4c128adbacc74af167d654c6b3c7884f
riotaro <rugby117117@gmail.com>
2026-06-23 09:47:02 +0900

    ゲームの流れを調節し、繰り返し処理は関数化

--- src/lib/black_jack/Game.php
+++ src/lib/black_jack/Game.php
@@ -39,35 +39,13 @@ class Game
     // 2回目ディーラーがカードを引く
     $dealer->addCardMyHand($deck);
     // 2回目ディーラーが引いたカードは表示しない
-    echo $message->dealerDrawSecondCardMessage($dealer, 'second') . PHP_EOL;
+    echo $message->dealerDrawSecondCardMessage($dealer, 'secret') . PHP_EOL;
 
-
-
-
-    
-    // プレイヤーの得点を表示して、カードを引くか聞く（ループが必要かも）TODO
-    echo $message->askDrawCardMessage($player) . PHP_EOL;
-
-    // Yならカードを1枚ひく、Nならカードを引かない
-    $response = $player->decideDrawAnotherCard();
-
-    $rule->addAnotherCard($response, $player, $deck);
-
-    // 引いたカードを表示する
-    echo $message->drawCardMessage($player) . PHP_EOL;
-
-    // プレイヤーの得点を表示して、カードを引くか聞く
-    // 21点以上ならカードを引くかどうか聞かなくてもいい
-    echo $message->askDrawCardMessage($player) . PHP_EOL;
-
-    // Yならカードを1枚ひく、Nならカードを引かない
-    $response = $player->decideDrawAnotherCard();
-
-    $rule->addAnotherCard($response, $player, $deck);
+    // 繰り返しの処理
+    $this->whetherPlayerDraw($message, $player, $rule, $deck);
 
     // ディーラーが2枚目に引いたカードを表示する
-    echo $message->dealerDrawSecondCardMessage($dealer, 'second') . PHP_EOL;
-
+    echo $message->dealerDrawSecondCardMessage($dealer, 'open') . PHP_EOL;
     // ディーラーの現在の得点を表示する
     echo $message->pointMessage($dealer) . PHP_EOL;
     // ディーラーは17点以上になるようにカードを引き続ける
@@ -75,16 +53,38 @@ class Game
     // 引いたカードを表示する(引きまくったら最後のカードしか表示されない)
     echo $message->drawCardMessage($dealer) . PHP_EOL;
 
-    // 結果発表
+    // 現在の得点発表
     echo $message->pointMessage($player) . PHP_EOL;
     echo $message->pointMessage($dealer) . PHP_EOL;
 
+    // 得点を比べて結果を出力
     $result = $rule->checkNotOverCloseTo21Points($dealer, $player);
-
+    // 結果発表
     echo $message->resultMessage($result) . PHP_EOL;
 
     echo 'ブラックジャックを終了します。' . PHP_EOL;
 
     return;
+  }
+
+  public function whetherPlayerDraw(Message $message, PlayerA $player, Rule $rule, Deck $deck): void
+  {
+    while ($player->totalScore() < 21) {
+
+      // プレイヤーの得点を表示して、カードを引くか聞く
+      echo $message->askDrawCardMessage($player) . PHP_EOL;
+
+      // Yならカードを1枚ひく、Nならカードを引かない
+      $response = $player->decideDrawAnotherCard();
+
+      if ($response == 'Y') {
+        $player->addCardMyHand($deck);
+      } else {
+        break;
+      }
+
+      // 引いたカードを表示する
+      echo $message->drawCardMessage($player) . PHP_EOL;
+    }
   }
 }
