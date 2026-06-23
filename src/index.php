7aaa41254ac5a7ae8b9efe1d0317036ff2272fc4
riotaro <rugby117117@gmail.com>
2026-06-24 07:07:05 +0900

    インデントの修正

--- src/index.php
+++ src/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once(__DIR__ . '/lib/black_jack/Game.php');
 
 use BlackJack\Game;
--- src/lib/black_jack/Card.php
+++ src/lib/black_jack/Card.php
@@ -1,16 +1,19 @@
 <?php
+
 namespace BlackJack;
 
 class Card
 {
-  public function __construct(public string $suit, public string $number) {}
+    public function __construct(public string $suit, public string $number)
+    {
+    }
 
-  public function getSuit(): string
-  {
-    return $this->suit;
-  }
-  public function getNumber(): string
-  {
-    return $this->number;
-  }
+    public function getSuit(): string
+    {
+        return $this->suit;
+    }
+    public function getNumber(): string
+    {
+        return $this->number;
+    }
 }
