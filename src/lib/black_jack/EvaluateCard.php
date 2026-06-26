d3f38095c81dd61b3a3a31b589befdb9fa70506f
riotaro <rugby117117@gmail.com>
2026-06-26 13:49:11 +0900

    Aceの配点を変更

--- src/lib/black_jack/EvaluateCard.php
+++ src/lib/black_jack/EvaluateCard.php
@@ -4,7 +4,7 @@ namespace BlackJack;
 
 class EvaluateCard
 {
-    public const ALL_CARDS_POINT = ['A' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10];
+    public const ALL_CARDS_POINT = ['2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11];
 
     // カードの点数を評価する
     public function evaluateCardPoint(Card $card): int
