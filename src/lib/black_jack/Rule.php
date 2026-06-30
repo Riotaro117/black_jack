b9d4f0aa04aeb65c397f89eb05ab41596e8c44f0
riotaro <rugby117117@gmail.com>
2026-06-30 15:26:40 +0900

    勝敗表示がわかりやすくなるように返り値の変更

--- src/lib/black_jack/Rule.php
+++ src/lib/black_jack/Rule.php
@@ -3,28 +3,33 @@
 namespace BlackJack;
 
 require_once(__DIR__ . '/Dealer.php');
-require_once(__DIR__ . '/PlayerA.php');
+require_once(__DIR__ . '/Player.php');
 
 class Rule
 {
     // どちらのスコアが21点に近いかチェック
-    public function checkNotOverCloseTo21Points(Dealer $dealer, PlayerA $player): string
+
+    /**
+     * @return array<int,string>
+     */
+    public function checkNotOverCloseTo21Points(Dealer $dealer, Player $player): array
     {
         $dealerTotalScore = $dealer->totalScore();
         $playerTotalScore = $player->totalScore();
 
+        // 配列の要素の0番目に勝者、1番目に敗者を入れる
         if ($playerTotalScore > 21) {
-            return 'dealer';
+            return [$dealer->getMyName(), $player->getMyName()];
         }
         if ($dealerTotalScore > 21) {
-            return 'player';
+            return [$player->getMyName(), $dealer->getMyName()];
         }
         if ($playerTotalScore > $dealerTotalScore) {
-            return 'player';
+            return [$player->getMyName(), $dealer->getMyName()];
         }
         if ($dealerTotalScore > $playerTotalScore) {
-            return 'dealer';
+            return [$dealer->getMyName(), $player->getMyName()];
         }
-        return 'draw';
+        return [];
     }
 }
